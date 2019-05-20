0caa224928b312ef7bb235919647a50fe884f02d
danialbleile <danial.bleile@wsu.edu>
2019-05-20 12:06:26 -0700

    build out objects and dependancies for main class

--- includes/classes/class-wsuwp-social-media.php
+++ includes/classes/class-wsuwp-social-media.php
@@ -5,6 +5,10 @@ class WSUWP_Social_Media {
 
 	private static $instance;
 
+	public $options;
+	public $social_metabox;
+	public $open_graph;
+
 
 	public static function get_instance() {
 
@@ -25,11 +29,22 @@ class WSUWP_Social_Media {
 
 	private function init_plugin() {
 
-	}
+		require_once __DIR__ . 'class-social-metabox.php';
+		require_once __DIR__ . 'class-open-graph.php';
+		require_once __DIR__ . 'class-options.php';
+
+		$this->options        = new Options();
+		$this->social_metabox = new Social_Metabox( $this->options );
+		$this->open_graph     = new Open_Graph( $this->options );
+
+	} // End init_plugin 
 
 
 	public function setup_plugin() {
 
+		$this->options->setup();
+		$this->social_metabox->setup();
+		$this->open_graph->setup();
 
 	} // End setup_plugin
 
