5f37c5d7aca0c17c7207e5fce7579a9d0f0f9cca
danialbleile <danial.bleile@wsu.edu>
2019-05-20 11:40:04 -0700

    Add plugin meta

---
+++ wsuwp-plugin-social-media.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: WSUWP Social Media Tools
+Version: 0.0.1
+Description: A plugin to add Social Media Tools.
+Author: washingtonstateuniversity, Danial Bleile
+Author URI: https://github.com/washingtonstateuniversity/
+Plugin URI: https://github.com/washingtonstateuniversity/wsuwp-plugin-social-media
+Text Domain: wsuwp-plugin-social-media
+Requires at least: 4.7
+Tested up to: 5.2.0
+Requires PHP: 5.3
+*/
