d13ff2316fdd9d5217715b05c35a7c7491dd103f
Danial Bleile <danbleile@gmail.com>
2019-06-12 10:45:32 -0700

    Update type to article.

--- includes/components/opengraph/opengraph.php
+++ includes/components/opengraph/opengraph.php
@@ -4,7 +4,7 @@
 <?php if ( ! empty( $search_snippet ) ) : ?><meta name="description" content="<?php echo esc_html( $search_snippet ); ?>"><?php endif; ?>
 <?php if ( ! empty( $search_keywords ) ) : ?><meta name="keywords" content=""><?php endif; ?>
 <?php if ( ! empty( $fb_title ) ) : ?><meta property="og:title" content="<?php echo esc_html( $fb_title ); ?>" /><?php endif; ?>
-<meta property="og:type" content="website" />
+<meta property="og:type" content="article" />
 <?php if ( ! empty( $fb_url ) ) : ?><meta property="og:url" content="<?php echo esc_url( $fb_url ); ?>" /><?php endif; ?>
 <?php if ( ! empty( $fb_snippet ) ) : ?><meta property="og:description" content="<?php echo esc_html( $fb_snippet ); ?>" /><?php endif; ?>
 <?php if ( ! empty( $fb_img_src ) ) : ?><meta property="og:image" content="<?php echo esc_url( $fb_img_src ); ?>" /><?php endif; ?>
